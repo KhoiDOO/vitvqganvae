c9a80ae41e82a66426694beb74383acb826f90fe
KhoiDOO <khoido8899@gmail.com>
2025-09-21 18:56:58 +0100

    api: export model pipeline and core classes in package init

--- vitvqganvae/model/__init__.py
+++ vitvqganvae/model/__init__.py
@@ -5,6 +5,7 @@ __all__ = [
 
     "Encoder",
     "Decoder",
+    "Discriminator",
     "VQVAE",
     "VQVAEConfig",
     "VAE",
@@ -23,6 +24,7 @@ from .hf import VQVAEPipeline, FlexVQVAEPipeline, VAEPipeline
 
 from .cnn.encoder import Encoder
 from .cnn.decoder import Decoder
+from .cnn.discriminator import Discriminator
 from .cnn.vqvae import VQVAE, VQVAEConfig
 from .cnn.variant import (
     FlexVQVAE, FlexVQVAEConfig
