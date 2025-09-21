b4d25b891737eeaedb82a5bb10825cd779325b6c
KhoiDOO <khoido8899@gmail.com>
2025-09-21 18:56:55 +0100

    test: update test script to use pytest for model tests

--- test.sh
+++ test.sh
@@ -6,8 +6,11 @@ python -m pytest test/model/cnn/test_dec_block.py
 python -m pytest test/model/cnn/test_encoder.py
 python -m pytest test/model/cnn/test_decoder.py
 
+python -m pytest test/model/cnn/test_vae.py
 python -m pytest test/model/cnn/test_vqvae.py
 python -m pytest test/model/cnn/test_flex_vqvae.py
+
+python -m pytest test/model/cnn/test_discriminator.py
 
 python -m pytest test/model/vit/test_img_encoder.py
 python -m pytest test/model/vit/test_img_decoder.py
